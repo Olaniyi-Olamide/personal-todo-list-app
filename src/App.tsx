f99db88062ffd1fab04534d5dc4c5fe1525a9840
Olaniyi Olamide <olamideolaniyi616@gmail.com>
2026-01-08 12:48:44 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <p>A boy is a male</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
